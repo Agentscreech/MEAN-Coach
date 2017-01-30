87adf1403f2ac4b7fa12060e256c6e0c5e6275fc
Travis Walentin <travis.walentin@gmail.com>
2017-01-30 12:08:49 -0800

    userSettings pages can calculate your BMR.  Need to work on updating User db with this info

--- static/app/containers/userSettings/userSettings.js
+++ static/app/containers/userSettings/userSettings.js
@@ -5,8 +5,30 @@ angular.module('App')
   controllerAs: 'userSettingsComp'
 });
 
-function UserSettingsCompCtrl() {
+function UserSettingsCompCtrl($state) {
+      var userSettingsComp = this;
 
+      userSettingsComp.weight = 70;
+      userSettingsComp.height = 180;
+      userSettingsComp.age = 21;
+      userSettingsComp.gender = 'male';
+      userSettingsComp.goal = 0;
+      userSettingsComp.bmr = false;
+
+   userSettingsComp.calcBMR = function (weight, height, age, gender) {
+        console.log('trying to calculate');
+        if (gender == 'Male'){
+          bmr = (10 * weight) + (6.25 * height) - (5* age) + 5;
+        } else if (gender == 'Female'){
+          bmr = (10 * weight) + (6.25 * height) - (5* age) - 161;
+        } else {
+          error = "gender not defined";
+          return error;
+        }
+        // console.log(bmr);
+        userSettingsComp.bmr = bmr;
+        // return bmr;
+      };
 }
 
-UserSettingsCompCtrl.$inject = [];
+UserSettingsCompCtrl.$inject = ['$state'];
