a0b70057506562a7bbdb2008292b15d9a1051fa4
Travis Walentin <travis.walentin@gmail.com>
2017-01-29 20:57:29 -0800

    broke out schemas to seperate files

--- static/app/containers/login/login.js
+++ static/app/containers/login/login.js
@@ -1,21 +1,21 @@
 angular.module('App')
-.component('loginComp', {
-  templateUrl: 'app/containers/login/login.html',
-  controller: LoginCompCtrl,
-  controllerAs: 'loginComp'
-});
+    .component('loginComp', {
+        templateUrl: 'app/containers/login/login.html',
+        controller: LoginCompCtrl,
+        controllerAs: 'loginComp'
+    });
 
 function LoginCompCtrl($scope, $state, UserService) {
-  $scope.user = {
-    email: '',
-    password: ''
-  };
-  $scope.userLogin = function() {
-    UserService.login($scope.user)
-    console.log("This is user logging in: ", $scope.user);
-    //Redirect home on successful login
-    $state.go('homeState');
-  }
+    $scope.user = {
+        email: '',
+        password: ''
+    };
+    $scope.userLogin = function() {
+        UserService.login($scope.user);
+        console.log("This is user logging in: ", $scope.user);
+        //Redirect home on successful login
+        $state.go('homeState');
+    };
 }
 
 LoginCompCtrl.$inject = ['$scope', '$state', 'UserService'];
