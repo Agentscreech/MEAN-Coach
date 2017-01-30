9d7d469b124e90d24452cf1f4027f2fee01cc758
Ryne Schillinger <ryneschillinger@gmail.com>
2017-01-30 14:40:29 -0800

    basic profile page layout

--- static/app/containers/profile/profile.js
+++ static/app/containers/profile/profile.js
@@ -5,10 +5,32 @@ angular.module('App')
   controllerAs: 'profileComp'
 });
 
-function ProfileCompCtrl($scope, $state, Auth) {
+function ProfileCompCtrl($scope, $state, Auth, $http, $window) {
   $scope.isLoggedIn = function() {
     return Auth.isLoggedIn();
   }
+
+  $scope.foodSearch = function() {
+    var req = {
+      url: 'http://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=b&format=json&api_key=voDReYpFIe0hJoOxgqqfGU28oUAf3Yp1HbsfOGEg',
+      method: "GET"
+    }
+    console.log("request: ", req);
+    // $window.location.href = req.url;
+
+    $http(req).then(function success(res) {
+    	console.log("response: ", res.data);
+    //   if (res.data.Error === "Movie not found!") {
+    //     $scope.results = [];
+    //   } else {
+    //     $scope.results = res.data.data;
+    //   }
+    // }, function failure(res) {
+    //   $scope.results = [];
+    });
+
+  };
 }
 
-ProfileCompCtrl.$inject = ['$scope', '$state', 'Auth'];
+
+ProfileCompCtrl.$inject = ['$scope', '$state', 'Auth', '$http', '$window'];
